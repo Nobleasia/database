a123d9f0644d8484755f6d0e87ba775badca4e05
Noble Asia <141794651+Nobleasia@users.noreply.github.com>
2023-09-01 10:16:01 +0700

    Update index.jsx

--- client/src/pages/apartment-management/index.jsx
+++ client/src/pages/apartment-management/index.jsx
@@ -265,7 +265,7 @@ const ApartementManagement = ({ showColumnFieldItems }) => {
                   className="h-full w-full text-center"
                   tabIndex={-1}
                 >
-                  Property Partialan
+                  Export
                 </Button>
               </Link>
               {auth?.user_role !== "user" && (
