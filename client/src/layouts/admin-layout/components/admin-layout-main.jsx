8690a45d08699d3e454587bc814924b676a8f14e
Xploratech <115635319+xplorationtechnology@users.noreply.github.com>
2024-09-30 15:16:02 +0800

    fix: scrolling issue

--- client/src/layouts/admin-layout/components/admin-layout-main.jsx
+++ client/src/layouts/admin-layout/components/admin-layout-main.jsx
@@ -1,6 +1,6 @@
 export const AdminLayoutMain = ({ children }) => {
   return (
-    <main className="flex h-screen min-h-max max-w-[100vw] flex-col gap-8 overflow-y-auto p-5 pb-48 sm:max-w-full lg:px-10">
+    <main className="flex h-max max-w-[100vw] flex-col gap-8 overflow-y-auto p-5 pb-48 sm:max-w-full lg:px-10">
       {children}
     </main>
   );
